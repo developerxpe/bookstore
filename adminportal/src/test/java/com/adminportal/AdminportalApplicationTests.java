3c7e915fcec71c82dceceaad00324951ac2bc446
developerxpe <selim223@aof.anadolu.edu.tr>
2022-10-28 22:08:37 +0300

    initial commit

---
+++ adminportal/src/test/java/com/adminportal/AdminportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adminportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
